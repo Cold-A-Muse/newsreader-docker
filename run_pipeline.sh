a7c72e6eca55f2e96ee6a70d6d0e9eb30b808274
Wouter van Atteveldt <wouter@vanatteveldt.com>
2017-02-23 21:38:51 +0800

    Initial checkin

---
+++ run_pipeline.sh
@@ -0,0 +1,6 @@
+set -e
+set -o pipefail
+
+java -jar ixa-pipe-tok/target/ixa-pipe-tok-1.8.5-exec.jar tok -l nl |\
+    /morphosyntactic_parser_nl/run_parser.sh |\
+    java -jar /ixa-pipe-nerc/target/ixa-pipe-nerc-1.6.1-exec.jar tag -m /models/nerc-models-1.5.4/nl/nl-6-class-clusters-sonar.bin
